34f9e873dccdc2aba82198ad43818edf6a45c1a4
param949 <param.bhawnai@st.niituniversity.in>
2019-09-13 12:47:04 +0530

    menu

---
+++ menu.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class clac{
+public static void main(String args[])
+{
+Scanner sc= new Scanner(System.in);
+
+int c= sc.nextInt();
+    
+   if(c==1){
+   #add}
+   else if(c==2){
+     #subtract}
+else if(c==3){
+#multiply}
+else if (c==4){
+divide}
+else  
+System.out.println ("enter the selected command");
+}
+
+}
