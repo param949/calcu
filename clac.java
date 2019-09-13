0389a537b920fd1571d1713d7c825b8143b40a93
param949 <param.bhawnai@st.niituniversity.in>
2019-09-13 12:39:18 +0530

    menu

--- clac.java
+++ clac.java
@@ -3,12 +3,19 @@ class clac{
 public static void main(String args[])
 {
 Scanner sc= new Scanner(System.in);
-System.out.println( "Enter the 1st no.");
-int a=sc.nextInt();
-System.out.println( "Enter the 2nd no.");
-int b=sc.nextInt();
-int sum=0;
- sum=a+b;
- System.out.println( "sum of 2 no ="+sum);
+
+int c= sc.nextInt();
+    
+   if(c==1){
+   #add}
+   else if(c==2){
+     #subtract}
+else if(c==3){
+#multiply}
+else if (c==4){
+divide}
+else  
+System.out.println ("enter the selected command");
 }
+
 }
