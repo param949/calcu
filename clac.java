ddc22883592a9f9aac1b79a330d4a317d5545ed3
param949 <param.bhawnai@st.niituniversity.in>
2019-09-13 12:27:09 +0530

    First commit

---
+++ clac.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class clac{
+public static void main(String args[])
+{
+Scanner sc= new Scanner(System.in);
+System.out.println( "Enter the 1st no.");
+int a=sc.nextInt();
+System.out.println( "Enter the 2nd no.");
+int b=sc.nextInt();
+int sum=0;
+ sum=a+b;
+ System.out.println( "sum of 2 no ="+sum);
+}
+}
